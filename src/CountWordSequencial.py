1c84bd2566388eaad8eab8114f066a312faefc81
jordi toda <jordicatalunya@hotmail.com>
2018-03-07 13:27:41 +0100

    secuencial

---
+++ src/CountWordSequencial.py
@@ -0,0 +1,52 @@
+
+def puntuation(paraula):
+    paraula = paraula.replace('*','')
+    paraula = paraula.replace('.','')
+    paraula = paraula.replace('-',' ')
+    paraula = paraula.replace('_',' ')
+    paraula = paraula.replace('?','')
+    paraula = paraula.replace('!','')
+    paraula = paraula.replace(',','')
+    paraula = paraula.replace('(','')
+    paraula = paraula.replace(')','')
+    paraula = paraula.replace('[','')
+    paraula = paraula.replace(']','')
+    paraula = paraula.replace('\'','')
+    paraula = paraula.replace('\"','')
+    paraula = paraula.replace('=',' ')
+    paraula = paraula.replace('#',' ')
+    paraula = paraula.replace(':',' ')
+    paraula = paraula.replace(';',' ')
+    paraula = paraula.replace('/',' ')
+    return paraula.lower()
+
+def add(paraula):
+    if (paraula not in diccionari):
+        diccionari[paraula] = 1
+    else:
+        aux = diccionari[paraula]
+        diccionari[paraula] = aux+1
+
+if __name__ == "__main__":
+
+    f = open ("sherlock.txt")
+    #f = open ("Quijote.txt")
+    #f = open ("bible.txt")
+    #f = open ("fitxeroProva.txt")
+    diccionari = {}
+
+    for line in f.readlines():
+        for paraula in line.split():
+            
+            paraula = puntuation(paraula)
+            
+            if(paraula.find(" ")>=0):
+                for p in paraula.split():
+                    add(p)
+
+            else:
+                add(paraula)
+
+    print diccionari
+    f.close()
+
---
+++ src/WordCountSecuencial.py
@@ -0,0 +1,44 @@
+
+
+def puntuation(paraula):
+    paraula = paraula.replace('*','')
+    paraula = paraula.replace('.','')
+    paraula = paraula.replace('-',' ')
+    paraula = paraula.replace('_',' ')
+    paraula = paraula.replace('?','')
+    paraula = paraula.replace('!','')
+    paraula = paraula.replace(',','')
+    paraula = paraula.replace('(','')
+    paraula = paraula.replace(')','')
+    paraula = paraula.replace('[','')
+    paraula = paraula.replace(']','')
+    paraula = paraula.replace('\'','')
+    paraula = paraula.replace('\"','')
+    paraula = paraula.replace('=',' ')
+    paraula = paraula.replace('#',' ')
+    paraula = paraula.replace(':',' ')
+    paraula = paraula.replace(';',' ')
+    paraula = paraula.replace('/',' ')
+    return paraula.lower()
+
+if __name__ == "__main__":
+
+    f = open ("sherlock.txt")
+    #f = open ("Quijote.txt")
+    #f = open ("bible.txt")
+    #f = open ("fitxeroProva.txt")
+    contador = 0
+
+    for line in f:
+        for paraula in line.split(' '):
+            paraula = puntuation(paraula)
+            
+            if(paraula.find(" ")>=0):
+                for p in paraula.split():
+                    contador+=1
+
+            else:
+                contador+=1
+
+    print contador
+    f.close()
