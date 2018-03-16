751e0a71f381b6efc645053b57a9f2754743737d
jordi toda <jordicatalunya@hotmail.com>
2018-03-16 19:05:06 +0100

    Reduce added

---
+++ src/Reduce.py
@@ -0,0 +1,9 @@
+from pyactor.context import set_context, create_host, serve_forever
+
+if __name__ == "__main__":
+    set_context()
+    host = create_host('http://127.0.0.1:1281/')
+    registry = host.lookup_url('http://127.0.0.1:6000/regis', 'RegistryM', 'Registry')
+
+    registry.bind('Reduce', host)
+    serve_forever()
